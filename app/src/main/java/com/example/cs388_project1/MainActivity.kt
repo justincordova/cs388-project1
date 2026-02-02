4e2b51d4d979455c535dd09c83b41285140681a2
Justin Cordova <justinavodroc@gmail.com>
2026-02-02 18:10:06 -0500

    feat: create MainActivity basic structure

--- app/src/main/java/com/example/cs388_project1/MainActivity.kt
+++ app/src/main/java/com/example/cs388_project1/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.cs388_project1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var guessTexts: Array<TextView>
+    private lateinit var resultTexts: Array<TextView>
+    private lateinit var guessInput: EditText
+    private lateinit var submitBtn: Button
+    private lateinit var resetBtn: Button
+    private lateinit var answerText: TextView
+
+    private var wordToGuess: String = ""
+    private var guessCount: Int = 0
+    private var isGameOver: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +31,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initializeUI()
+        wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+        setupButtons()
+    }
+
+    private fun initializeUI() {
+    }
+
+    private fun setupButtons() {
     }
 }
